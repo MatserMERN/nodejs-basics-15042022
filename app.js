d84a8dbf9aef568846c8e9b5555c7ce4225665c3
UmamaheshMaxwell <maheshmeka16@gmail.com>
2022-04-20 17:07:03 +0530

    added nodejs basic setup

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Here we go, We are in the world of NodeJS")
+
+var a =10
+var b =20
+console.log(a+b)
+
+function sample(){
+    return " I am running my javascript code using NodeJS"
+}
+
+console.log(sample())
+
+var user = {
+    id: 1
+}
+
+console.log(user)
+
+const names = ["Scott", "Adam", "Tuan", "Uma"]
+console.log(names)
+
+console.log("Now nodemon will run our code automatically - So Cool")
